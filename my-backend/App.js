9c6fe4390ca5d368b942824e3493aa397c3d9ea8
abnerbal <abnersinghbal@EMAIL.com>
2024-03-24 11:07:15 -0700

    First commit

--- my-backend/App.js
+++ my-backend/App.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use(cors());
@@ -54,6 +54,7 @@ app.get('/business', (req, res) => {
         res.json(businesses);
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
